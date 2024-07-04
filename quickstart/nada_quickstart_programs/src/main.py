435de16ec0323ad7be0c245a6f521ce3b291abdb
Rhythm-Shahane <rhythm.shahane2020@gmail.com>
2024-07-04 11:18:48 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -2,22 +2,30 @@ from nada_dsl import *
 
 def nada_main():
     # Initialize parties
-    alice = Party(name="Alice")
-    bob = Party(name="Bob")
-    charlie = Party(name="Charlie")
+    abhishek = Party(name="Abhishek")
+    devansh = Party(name="Devansh")
+    prathamesh = Party(name="Prathamesh")
 
     # Initialize secret integer inputs for each party's salary
-    alice_salary = SecretInteger(Input(name="alice_salary", party=alice))
-    bob_salary = SecretInteger(Input(name="bob_salary", party=bob))
-    charlie_salary = SecretInteger(Input(name="charlie_salary", party=charlie))
+    abhishek_salary = SecretInteger(Input(name="abhishek_salary", party=abhishek))
+    devansh_salary = SecretInteger(Input(name="devansh_salary", party=devansh))
+    prathamesh_salary = SecretInteger(Input(name="prathamesh_salary", party=prathamesh))
 
-    # Determine the position of the party with the lowest salary using conditional expressions
-    lowest_position = (alice_salary < bob_salary).if_else(
-        (alice_salary < charlie_salary).if_else(Integer(0), Integer(2)),
-        (bob_salary < charlie_salary).if_else(Integer(1), Integer(2)),
+    # Determine the position of the party with the highest salary using conditional expressions
+    highest_position = (abhishek_salary > devansh_salary).if_else(
+        (abhishek_salary > prathamesh_salary).if_else(Integer(0), Integer(2)),
+        (devansh_salary > prathamesh_salary).if_else(Integer(1), Integer(2)),
     )
 
-    # Output the position of the party with the lowest salary
-    out = Output(lowest_position, "lowest_position", alice)
+    # Determine the position of the party with the lowest salary using conditional expressions
+    lowest_position = (abhishek_salary < devansh_salary).if_else(
+        (abhishek_salary < prathamesh_salary).if_else(Integer(0), Integer(2)),
+        (devansh_salary < prathamesh_salary).if_else(Integer(1), Integer(2)),
+    )
 
-    return [out]
+    # Output the position of the party with the highest salary
+    highest_out = Output(highest_position, "highest_position", abhishek)
+    # Output the position of the party with the lowest salary
+    lowest_out = Output(lowest_position, "lowest_position", abhishek)
+
+    return [highest_out, lowest_out]
